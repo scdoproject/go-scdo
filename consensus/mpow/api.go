755934450db577f6cc327f2a23b58eb07da30f46
shawnxin89 <xzixing@seelenet.com>
2020-09-06 20:46:30 -0700

    add debtrate api

--- consensus/mpow/api.go
+++ consensus/mpow/api.go
@@ -9,6 +9,11 @@ type API struct {
 	engine *MpowEngine
 }
 
+// GetDetrate returns the current detrate for local CPU miner and remote miner.
+func (api *API) GetDetrate() uint64 {
+	return uint64(api.engine.detrate.Rate1())
+}
+
 // GetThreads returns the thread number of the miner engine
 func (api *API) GetThreads() int {
 	return api.engine.threads
