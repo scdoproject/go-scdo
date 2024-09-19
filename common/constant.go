1e557877c5b6ed876059734d39ce058f340f981b
elcn <elcn@mail.com>
2024-09-19 14:13:27 +0800

    Version 2.0.0

--- common/constant.go
+++ common/constant.go
@@ -22,7 +22,7 @@ const (
 	ScdoVersion uint = 1
 
 	// ScdoNodeVersion for simpler display
-	ScdoNodeVersion string = "Scdo_V1.0.0"
+	ScdoNodeVersion string = "Scdo_V2.0.0"
 
 	// ShardCount represents the total number of shards.
 	ShardCount = 4
